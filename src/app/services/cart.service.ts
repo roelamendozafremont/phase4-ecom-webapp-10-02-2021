2b17310bfe05ae6369948f90a014b66072603574
Roel Mendoza <64776122+roelamendozafremont@users.noreply.github.com>
2022-01-09 23:29:11 -0800

    Update cart.service.ts

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CartService {
 
-  private url: string = 'http://localhost:8090/api/carts';
+  private url: string = 'http://18.222.204.254:8090/api/carts';
 
   constructor(private httpClient: HttpClient) { }
 
